ffa9a7ea8b9ec5c3f9316463b5dcb1f94bf6dfe8
Hanaf Othmane <ohanaf@users.noreply.github.com>
2021-01-18 05:47:46 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fname = "Angela";
+const lname = "Yu";
+const number = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name + " " + lname}</h1>
+    <p>Your lucky number is {number}</p>
+  </div>,
+  document.getElementById("root")
+);
